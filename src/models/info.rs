8897e92a37ee9436da851f89f7f92addb231ffa0
Enoki <enokiun@gmail.com>
2022-09-03 17:28:32 +0300

    feat: re-exported todel::models::Info::info in todel::models

--- src/models/info.rs
+++ src/models/info.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+/// The instance info payload.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Info {
     pub instance_name: String,
