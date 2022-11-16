e5e973e703abd48704645ca708f57b974fb08b6e
Enoki <enokiun@gmail.com>
2022-11-16 12:15:12 +0300

    docs: document gateway::Payload

--- src/models/gateway.rs
+++ src/models/gateway.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use super::Message;
 
+/// The Pandemonium Payload Enum
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 #[serde(tag = "op", content = "d")]
