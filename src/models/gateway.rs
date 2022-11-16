feb6c23f7c4ea1ab1387f8ff907eaf20127618c5
Enoki <enokiun@gmail.com>
2022-11-16 12:04:39 +0300

    feat: add base gateway payloads

---
+++ src/models/gateway.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+use super::Message;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[serde(tag = "op", content = "d")]
+enum PandemoniumPayload {
+    Ping,
+    Message(Message),
+}
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,9 +1,11 @@
 //! A collection of models and some related function implementations for eludris.
 
+mod gateway;
 mod info;
 mod messages;
 mod ratelimits;
 
+pub use gateway::*;
 pub use info::*;
 pub use messages::*;
 pub use ratelimits::*;
