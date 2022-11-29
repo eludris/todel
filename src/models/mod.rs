5ebeb8e1875e5ef280896541a904f8fcd57ca3f4
Enoki <enokiun@gmail.com>
2022-11-29 19:28:47 +0300

    chore: re-export files.rs

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,11 +1,13 @@
 //! A collection of models and some related function implementations for eludris.
 
+mod files;
 mod gateway;
 mod info;
 mod messages;
 mod ratelimits;
 mod response;
 
+pub use files::*;
 pub use gateway::*;
 pub use info::*;
 pub use messages::*;
