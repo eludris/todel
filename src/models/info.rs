3d3c0de21690fe91d2779fb8b12934bd74f17124
Enoki <enokiun@gmail.com>
2022-11-29 22:48:06 +0300

    feat: change Info model name to InstanceInfo

--- src/models/info.rs
+++ src/models/info.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// The instance info payload.
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Info {
+pub struct InstanceInfo {
     pub instance_name: String,
     // This should be somewhere between 1 character and 2048 characters long
     pub description: Option<String>,
