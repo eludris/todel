25e065b5ebb3a935f477b25678fab02b2b666aaa
Enoki <enokiun@gmail.com>
2022-11-30 23:48:28 +0300

    feat: add urls for microservices in config and info

--- src/models/info.rs
+++ src/models/info.rs
@@ -7,4 +7,10 @@ pub struct InstanceInfo {
     // This should be somewhere between 1 character and 2048 characters long
     pub description: Option<String>,
     pub message_limit: usize,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub oprish_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pandemonium_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub effis_url: Option<String>,
 }
